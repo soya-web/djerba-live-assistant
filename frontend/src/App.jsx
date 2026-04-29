461b546194a263db7cc722beefeed260248cca67
soya-web <ecom.ops.pro@gmail.com>
2026-04-29 09:31:12 +0200

    Update App.jsx to include WeatherDashboard component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import WeatherDashboard from './WeatherDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <WeatherDashboard />
+      {/* Other components */}
+    </div>
+  );
+}
+
+export default App;
